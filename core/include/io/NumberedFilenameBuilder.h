784188741debfa7129f1b8f3dbe95bf5974cfa2c
Jiawen Chen <jiawen@csail.mit.edu>
2012-08-19 18:54:59 -0700

    New class: NumberedFilenameBuilder for building numbered filename sequences with prefixes and fixed width 0 padding.
    PerspectiveCamera: constructor now wants angles in radians.
    GeometryUtils::rayTriangleIntersection: returns barycentrics as a Vector3f.

---
+++ core/include/io/NumberedFilenameBuilder.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <QString>
+
+class NumberedFilenameBuilder
+{
+public:
+
+	// Construct a NumberedFilenameBuilder for a sequence of files
+	//
+	// e.g.,
+	//   prefix = "/tmp/frame_"
+	//   suffix = ".png"
+	//   output = "/tmp/frame_00000.png"
+	//
+	// prefix cannot contain the string "%1" as a substring
+	// (a bad idea for filenames anyway...)
+	NumberedFilenameBuilder( QString prefix, QString suffix, int nDigits = 5 );
+
+	QString filenameForNumber( int number );
+
+private:
+
+	int m_nDigits;
+	QString m_baseString;
+
+};
---
+++ core/src/io/NumberedFilenameBuilder.cpp
@@ -0,0 +1,14 @@
+#include "io/NumberedFilenameBuilder.h"
+
+NumberedFilenameBuilder::NumberedFilenameBuilder( QString prefix, QString suffix, int nDigits ) :
+
+	m_nDigits( nDigits )
+
+{
+	m_baseString = prefix + "%1" + suffix;
+}
+
+QString NumberedFilenameBuilder::filenameForNumber( int number )
+{
+	return m_baseString.arg( number, m_nDigits, QChar( '0' ) );
+}
